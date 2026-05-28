60d5963c25fd578d2df2b46aab8a85d03e7e1bb5
Nedissa <nedalissa@outlook.com>
2026-05-28 18:37:39 +0200

    Dölj headern på inlogg-sidan

--- apps/frontend/app/components/RootLayoutClient.tsx
+++ apps/frontend/app/components/RootLayoutClient.tsx
@@ -5,11 +5,15 @@ import { CartAside } from './CartAside';
 import React from 'react';
 import { HeaderWrapper } from './HeaderWrapper';
 import { FooterWrapper } from './FooterWrapper';
+import { usePathname } from 'next/navigation';
 
 export function RootLayoutClient({ children }: { children: React.ReactNode }) {
+  const pathname = usePathname();
+  const hideHeader = pathname === '/inlogg';
+
   return (
     <Aside.Provider>
-      <HeaderWrapper />
+      {!hideHeader && <HeaderWrapper />
       <main className="py-4 pb-24 flex justify-center flex-1 min-h-screen">
         <div className="w-full max-w-[1280px]">
           {children}
