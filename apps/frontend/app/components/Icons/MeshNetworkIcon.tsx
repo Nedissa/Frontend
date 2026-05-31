13a6ed86f90470080c2945fd20d4a2a100763483
Nedissa <nedalissa@outlook.com>
2026-05-31 12:03:41 +0200

    Fix network icons with proper SVG paths

--- apps/frontend/app/components/Icons/MeshNetworkIcon.tsx
+++ apps/frontend/app/components/Icons/MeshNetworkIcon.tsx
@@ -1,8 +1,7 @@
 export function MeshNetworkIcon() {
   return (
-    <svg className="w-6 h-6" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-      <path fill="currentColor" d="M12 2a2 2 0 1 1 0 4 2 2 0 0 1 0-4zm-7 9a2 2 0 1 1 0 4 2 2 0 0 1 0-4zm14 0a2 2 0 1 1 0 4 2 2 0 0 1 0-4zm-7 7a2 2 0 1 1 0 4 2 2 0 0 1 0-4zm-4.5-6.5l3-3m3 0l3 3m-9 5l3 3m3-3l3 3"/>
-      <path fill="none" stroke="currentColor" strokeWidth="1.5" d="M7.5 10.5l3-3m3 0l3 3M7.5 13.5l3 3m3-3l3 3"/>
+    <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+      <path d="M12 3C9.24 3 6.75 4.08 4.93 5.87L3.5 4.43C5.69 2.27 8.7 1 12 1s6.31 1.27 8.5 3.43l-1.43 1.44C17.25 4.08 14.76 3 12 3zm0 4c-1.65 0-3.15.67-4.24 1.76L6.34 7.34C7.8 5.9 9.79 5 12 5s4.2.9 5.66 2.34l-1.42 1.42C15.15 7.67 13.65 7 12 7zm0 4c-.55 0-1 .45-1 1s.45 1 1 1 1-.45 1-1-.45-1-1-1zm-7 7h14v2H5v-2zm2-4h10v2H7v-2z"/>
     </svg>
   );
 }
